54990c60fe15fedd08d92612affeb2d4ac1e7e07
Aaron Case <acase@mbacase.austin.kmvtechnologies.com>
2015-01-05 09:49:02 -0600

    staged

---
+++ js/fade.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+   $('h3').hide();
+   $('.fourth').hide();
+   $('.first').fadeIn(1500, function(){
+   	$('.second').fadeIn(1500, function(){
+   		$('.third').fadeIn(1500, function(){
+   			$('.fourth').fadeIn(1500)
+   		});
+   	});
+   });
+
+
+   $('#1').hide();
+   $('#2').hide();
+   $('#3').hide();
+   $('#4').hide();
+  
+});
+
+   $(window).scroll(function() {
+
+    if ($(this).scrollTop()>0)
+     {
+        
+         $('#1').fadeIn(1500, function(){
+         $('#2').slideDown(1500, function(){
+              $('#3').fadeIn(1500,function(){
+                $('#4').fadeIn(1500, function(){
+
+                $( 'div' ).removeClass("placeholder");
+                });
+              });
+         });
+     });
+     }
+    // else
+    //  {
+    //   $('.a').fadeIn();
+    //  }
+ });
